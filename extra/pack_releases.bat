5a58899e86401f1fa4e74dee59122a67581427ee
Ariman <ariman@inbox.ru>
2014-05-23 17:42:49 +0400

    Updated packing script.

--- extra/pack_releases.bat
+++ extra/pack_releases.bat
@@ -4,13 +4,19 @@ IF "%1" == "" GOTO :EMPTY_VERSION
 
 SET PACKER_CMD=@rar.exe a -y -r -ep1 -apIntChecker2
 
+:Far2
+IF NOT EXIST ..\bin\Release-Far2\ GOTO Far2x64
 ECHO Packing Far2 x86
 %PACKER_CMD% -- ..\bin\IntChecker2_Far2_x86_%1.rar "..\bin\Release-Far2\*" > nul
 if NOT ERRORLEVEL == 0 GOTO PACK_ERROR
 
+:Far2x64
+IF NOT EXIST ..\bin\Release-Far2-x64\ GOTO Far3
 ECHO Packing Far2 x64
 %PACKER_CMD% -- ..\bin\IntChecker2_Far2_x64_%1.rar "..\bin\Release-Far2-x64\*" > nul
 if NOT ERRORLEVEL == 0 GOTO PACK_ERROR
+
+:Far3
 
 ECHO [SUCCESS]
 EXIT 0
