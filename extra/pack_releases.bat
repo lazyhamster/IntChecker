ed85825cf980dc5e172a1e9d3526a3a22c5e7c9e
Ariman <ariman@inbox.ru>
2018-06-01 12:25:59 +0300

    Identical template for output folder name for all platforms.

--- extra/pack_releases.bat
+++ extra/pack_releases.bat
@@ -32,25 +32,25 @@ SET SOLUTION_FILE=..\IntChecker2.sln
 
 :Far2
 ECHO Building version for Far 2 x86
-call :Build_Version Far2 Win32 Release-Far2 x86
+call :Build_Version Far2 Win32 x86
 if ERRORLEVEL == 1 GOTO BUILD_ERROR
 if ERRORLEVEL == 2 GOTO PACK_ERROR
 
 :Far2x64
 ECHO Building version for Far 2 x64
-call :Build_Version Far2 x64 Release-Far2-x64 x64
+call :Build_Version Far2 x64 x64
 if ERRORLEVEL == 1 GOTO BUILD_ERROR
 if ERRORLEVEL == 2 GOTO PACK_ERROR
 
 :Far3
 ECHO Building version for Far 3 x86
-call :Build_Version Far3 Win32 Release-Far3 x86
+call :Build_Version Far3 Win32 x86
 if ERRORLEVEL == 1 GOTO BUILD_ERROR
 if ERRORLEVEL == 2 GOTO PACK_ERROR
 
 :Far3x64
 ECHO Building version for Far 3 x64 
-call :Build_Version Far3 x64 Release-Far3-x64 x64
+call :Build_Version Far3 x64 x64
 if ERRORLEVEL == 1 GOTO BUILD_ERROR
 if ERRORLEVEL == 2 GOTO PACK_ERROR
 
@@ -59,17 +59,19 @@ ECHO [SUCCESS]
 EXIT 0
 
 :Build_Version
-REM Arguments <Far Version> <Platform Name> <Bin Folder Name> <Archive Platform Name>
+REM Arguments <Far Version> <Platform Name> <Archive Platform Name>
 
 ECHO Executing build
 %DEVENV_EXE_PATH% /Rebuild "Release-%~1|%~2" "%SOLUTION_FILE%"
-IF NOT EXIST ..\bin\%~3\ EXIT /B 1
+IF NOT EXIST ..\bin\Release-%~1-%~2\ EXIT /B 1
 ECHO Packing archive
-rar.exe a -y -r -ep1 -apIntChecker2 -- ..\bin\IntChecker2_%~1_%~4_%PVER%.rar "..\bin\%~3\*" .\*.lua > nul
+SET SCRIPTS=
+IF "%~1" == "Far3" SET SCRIPTS=.\*.lua
+rar.exe a -y -r -ep1 -apIntChecker2 -- ..\bin\IntChecker2_%~1_%~3_%PVER%.rar "..\bin\Release-%~1-%~2\*" %SCRIPTS% > nul
 if NOT ERRORLEVEL == 0 EXIT /B 2
 ECHO Cleanup
-rmdir /s /q "..\bin\%~3\"
-rmdir /s /q "..\obj\%~3\"
+rmdir /s /q "..\bin\Release-%~1-%~2\"
+rmdir /s /q "..\obj\Release-%~1-%~2\"
 ECHO.
 EXIT /B 0
 
