46a6e6db04e2358de74d3eeeb4175ddaa9b0894e
Vitaliy Didik <ariman@inbox.ru>
2019-04-23 11:04:39 +0300

    Updated Run.lua script.

--- extra/scripts/IntChecker.Run.lua
+++ extra/scripts/IntChecker.Run.lua
@@ -49,6 +49,13 @@
 -- 18.03.2019 10:35:23 +0300
 -- v1.4.2 - уточнение v1.4.1
 -- 18.03.2019 13:48:54 +0300
+-- v1.4.3 - при непустой командной строке если курсор стоит на хэше спросим
+-- оператора что делать? Ok - выполним командную строку, Canсel - проверим хэш.
+-- 23.03.2019 22:41:07 +0300
+-- v1.4.4 - мелкий рефакторинг, вопрос к пользователю задаётся по русски,
+-- а чтобы LuaCheck не ругался на длинную строку в mf.msgbox вынесем сообщение
+-- в отдельный оператор local так, чтобы вывелась только одна строка
+-- 22.04.2019 17:53:12 +0300
 
 local ICID="E186306E-3B0D-48C1-9668-ED7CF64C0E65";
 local ICMID="A22F9043-C94A-4037-845C-26ED67E843D1";
@@ -56,6 +63,7 @@ local Mask="/.+\\.(md5|sfv|sha(1|3|256|512)|wrpl)/i";
 local arg,algo,cmd,ext,hash,mod,pt,prn,tp;
 local MsB=Mouse.Button;
 local MsF=Mouse.EventFlags;
+local Msg="Командая строка не пуста, но под курсором хэш файл. Что выполнить? Команду: Ok или Проверку: Cancel";
 
 Macro{
   id="C7BD288F-E03F-44F1-8E43-DC7BC7CBE4BA";
@@ -66,7 +74,16 @@ Macro{
   flags="EnableOutput";
   condition=function() return (mf.fmatch(APanel.Current,Mask)==1 and not (MsB==0x0001 and MsF==0x0001)) end;
   action=function()
-    Far.DisableHistory(-1) Plugin.Command(ICID,APanel.Current);
+  if not CmdLine.Empty then
+ if mf.msgbox("Вопрос к пользователю:",Msg,0x00020000) == 1
+        then
+          Keys("Enter");
+        else
+          Far.DisableHistory(-1) Plugin.Command(ICID,APanel.Current);
+        end;
+    else
+      Far.DisableHistory(-1) Plugin.Command(ICID,APanel.Current);
+   end;
   end;
 }
 
